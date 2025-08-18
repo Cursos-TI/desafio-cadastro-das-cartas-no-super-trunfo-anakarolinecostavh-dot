4645dddb26a6028aa52894eac4f6fa8b8c0b080e
anakarolinecostavh-dot <anakarolinecostavh@gmail.com>
2025-08-18 02:37:20 +0000

    Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,8 @@ int main() {
     float carta1Area;
     float carta1Pib;
     int carta1Ponto;
+    float carta1DensidadePopulacional;
+    float carta1PibPercapita;
 
     char carta2Estado;
     char carta2Codigo[10];
@@ -16,16 +18,19 @@ int main() {
     float carta2Area;
     float carta2Pib;
     int carta2Ponto;
+    float carta2DensidadePopulacional;
+    float carta2PibPercapita;
 
     printf("Carta 1\n");
     printf("Digite uma letra de 'A' a 'H' (representando um dos oito estados): ");
     scanf(" %c", &carta1Estado);
+    getchar();
 
     printf("Digite a letra do estado seguida de um número de 01 a 04 (ex: A01, B03): ");
-    scanf("%s", carta1Codigo);
+    scanf("%s", &carta1Codigo);
 
     printf("Digite o nome da cidade: ");
-    scanf(" %[^\n]", carta1Cidade);
+    scanf(" %s", &carta1Cidade);
 
     printf("Digite o número de habitantes da cidade: ");
     scanf("%d", &carta1Populacao);
@@ -33,21 +38,23 @@ int main() {
     printf("Digite a área da cidade em quilômetros quadrados: ");
     scanf("%f", &carta1Area);
 
-    printf("Digite o Produto Interno Bruto da cidade: ");
+    printf("Digite o PIB da cidade: ");
     scanf("%f", &carta1Pib);
 
     printf("Digite a quantidade de pontos turísticos na cidade: ");
     scanf("%d", &carta1Ponto);
+    getchar();
 
     printf("\nCarta 2\n");
     printf("Digite uma letra de 'A' a 'H' (representando um dos oito estados): ");
     scanf(" %c", &carta2Estado);
+    getchar();
 
     printf("Digite a letra do estado seguida de um número de 01 a 04 (ex: A01, B03): ");
-    scanf("%s", carta2Codigo);
+    scanf("%s", &carta2Codigo);
 
     printf("Digite o nome da cidade: ");
-    scanf(" %[^\n]", carta2Cidade);
+    scanf(" %s", &carta2Cidade);
 
     printf("Digite o número de habitantes da cidade: ");
     scanf("%d", &carta2Populacao);
@@ -55,11 +62,12 @@ int main() {
     printf("Digite a área da cidade em quilômetros quadrados: ");
     scanf("%f", &carta2Area);
 
-    printf("Digite o Produto Interno Bruto da cidade: ");
+    printf("Digite o PIB da cidade: ");
     scanf("%f", &carta2Pib);
 
     printf("Digite a quantidade de pontos turísticos na cidade: ");
     scanf("%d", &carta2Ponto);
+    getchar();
 
     printf("\n--- CARTA 1 ---\n");
     printf("Estado: %c\n", carta1Estado);
@@ -78,6 +86,24 @@ int main() {
     printf("Área: %.2f km²\n", carta2Area);
     printf("PIB: %.2f bilhões de reais\n", carta2Pib);
     printf("Número de Pontos Turísticos: %d\n", carta2Ponto);
+
+    carta1DensidadePopulacional = carta1Populacao / carta1Area;
+    carta1PibPercapita = carta1Pib / carta1Populacao;
+
+    carta2DensidadePopulacional = carta2Populacao / carta2Area;
+    carta2PibPercapita = carta2Pib / carta2Populacao;
+
+    printf("\nComparação de cartas (Atributo: Densidade populacional): \n\n");
+    printf("Carta 1 - %s: %.2f \n", carta1Cidade, carta1DensidadePopulacional);
+    printf("Carta 2 - %s: %.2f \n\n", carta2Cidade, carta2DensidadePopulacional);
+
+    if (carta1DensidadePopulacional < carta2DensidadePopulacional) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", carta1Cidade);
+    } else if (carta1DensidadePopulacional > carta2DensidadePopulacional) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", carta2Cidade);
+    } else {
+        printf("Resultado: Cartas 1 (%s) e 2 (%s) empataram!!\n", carta1Cidade, carta2Cidade);
+    }
 
     return 0;
 }
